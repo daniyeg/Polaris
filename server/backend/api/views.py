24a5ef98cadaa0d5f3e0dcb3cd1d6eed14296278
Reza Soleymani <106319850+RezaSoleymani2003@users.noreply.github.com>
2025-06-08 15:56:24 +0430

    feat: sign up and login and deploy

---
+++ server/backend/api/views.py
@@ -0,0 +1,149 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from drf_yasg import openapi
+from polaris.models import *
+from .serializers import *
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from utils.sms import *
+from django.core.cache import cache
+from django.http import JsonResponse
+from django.contrib.auth import login
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import logout
+
+
+@swagger_auto_schema(method='post', request_body=RequestOTPSerializer)
+@api_view(['POST'])
+def request_otp(request):
+
+    serializer = RequestOTPSerializer(data=request.data)
+    
+    if not serializer.is_valid():
+        return JsonResponse(serializer.errors, status=400)
+    
+    phone_number = serializer.validated_data['phone_number']
+    
+
+    code, response = send_otp(phone_number)
+    
+    cache_key = f"otp_{phone_number}"
+    cache.set(cache_key, code, timeout=300)
+
+    return JsonResponse({
+        'status': 'success',
+        'message': 'OTP sent successfully',
+        'phone_number': str(phone_number),
+        'otp': code 
+    })
+
+
+@swagger_auto_schema(method='post', request_body=VerifyOTPSerializer)
+@api_view(['POST'])
+def verify_otp(request):
+    serializer = VerifyOTPSerializer(data=request.data)
+    
+    if not serializer.is_valid():
+        return JsonResponse(serializer.errors, status=400)
+    
+    phone_number = str(serializer.validated_data['phone_number'])
+    otp_code = serializer.validated_data['otp_code']
+    
+    cache_key = f"otp_{phone_number}"
+    cached_otp = cache.get(cache_key)
+    
+    if not cached_otp:
+        return JsonResponse({
+            'status': 'error',
+            'message': 'OTP expired or not requested'
+        }, status=400)
+    
+    if cached_otp != otp_code:
+        return JsonResponse({
+            'status': 'error',
+            'message': 'Invalid OTP'
+        }, status=400)
+     
+
+    try:
+        user = User.objects.get(phone_number=phone_number)
+        created = False
+    except User.DoesNotExist:
+        user_data = {
+            'phone_number': phone_number,
+            'username': phone_number,  
+        }
+
+    cache.delete(cache_key)
+
+    return JsonResponse({
+        'status': 'success',
+        'message': 'OTP verified successfully',
+        'phone_number': str(phone_number)
+    })
+
+
+@swagger_auto_schema(method='post', request_body=SignupSerializer)
+@api_view(['POST'])
+def signup_user(request):
+    serializer = SignupSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    user = serializer.save()
+    
+    return JsonResponse({
+        'status': 'success',
+        'message': 'User created successfully',
+        'user_id': user.id,
+        'username': user.username
+    })
+
+
+@swagger_auto_schema(method='post', request_body=LoginSerializer)
+@api_view(['POST'])
+def login_user(request):
+    serializer = LoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    user = serializer.validated_data['user']
+    login(request, user)  
+
+    token, _ = Token.objects.get_or_create(user=user)
+
+    return Response({
+        "message": "Login successful",
+        "token": token.key,
+        "user": {
+            "username": user.username,
+            "phone_number": user.phone_number,
+        }
+    })
+
+
+@permission_classes([IsAuthenticated])
+@api_view(['POST'])
+def logout_user(request):
+    logout(request)
+    return Response({"message": "Logged out successfully."})
+
+
+@swagger_auto_schema(method='get')
+@api_view(['GET'])
+def get_item(request):
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(method='post', request_body=ItemSerializer)
+@api_view(['POST'])
+def add_item(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
