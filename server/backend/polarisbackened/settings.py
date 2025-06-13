2531b8d6058ec2a32e3a303c078508f9c3b3f135
Reza Soleymani <106319850+RezaSoleymani2003@users.noreply.github.com>
2025-06-13 11:12:06 +0430

    fix deploy

--- server/backend/polarisbackened/settings.py
+++ server/backend/polarisbackened/settings.py
@@ -58,9 +58,9 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
