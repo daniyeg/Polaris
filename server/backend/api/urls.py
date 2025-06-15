af2ccaa32f5d2fb95ca1b52a9b357c5e9253a91b
Reza Soleymani <106319850+RezaSoleymani2003@users.noreply.github.com>
2025-06-15 10:09:54 +0430

    refine: api links

--- server/backend/api/urls.py
+++ server/backend/api/urls.py
@@ -3,13 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.get_item),
-    path('add/', views.add_item),
-    path('request_otp/', views.request_otp),
-    path('verify_otp/', views.verify_otp),
-
-    path('signup/', views.signup_user),
-    path('login/', views.login_user),
-    path('logout/', views.logout_user),
-
+    path('', views.get_item, name='get-item'),
+    path('add/', views.add_item, name='add-item'),
+    path('request_otp/', views.request_otp, name='request-otp'),
+    path('verify_otp/', views.verify_otp, name='verify-otp'),
+    
+    path('signup/', views.signup_user, name='signup'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
